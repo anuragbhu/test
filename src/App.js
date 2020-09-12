2dca2f83b094d6215b078c80296abd5e0617c17b
anuragbhu <anurag.bhumca18@gmail.com>
2020-09-12 19:12:34 +0530

    Final commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>This is the test Application for developing React Application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
